f7829cc41541b0ed654f7f0f080e531727c9044e
Tymur Krasnopir <krasnopir.std@gmail.com>
2025-09-04 13:11:25 +0300

    Handling form submission

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,10 @@
 function Form() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert("handleSubmit");
+  }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
         <h2 className="label-wrapper">
           <label htmlFor="new-todo-input" className="label__lg">
             What needs to be done?
